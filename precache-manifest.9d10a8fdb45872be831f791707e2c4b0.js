bec32cf5e2a68babc888d994dc070aa30576d179
D3c0y0ctopus <chris.ramsden85@gmail.com>
2024-01-10 21:31:03 +0000

    Updates

---
+++ precache-manifest.9d10a8fdb45872be831f791707e2c4b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52d9255f2bfb7e2aa7f9240a12393c9",
+    "url": "/calorie-counter-web/index.html"
+  },
+  {
+    "revision": "e421e852ce37480d0cbb",
+    "url": "/calorie-counter-web/static/css/main.fc720e3e.chunk.css"
+  },
+  {
+    "revision": "294cf96c9cfa8a48d13c",
+    "url": "/calorie-counter-web/static/js/2.adcedab8.chunk.js"
+  },
+  {
+    "revision": "b35683604c9ad9a2bcd08581d0015451",
+    "url": "/calorie-counter-web/static/js/2.adcedab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07de381080896f7c0346",
+    "url": "/calorie-counter-web/static/js/3.e1abf215.chunk.js"
+  },
+  {
+    "revision": "e421e852ce37480d0cbb",
+    "url": "/calorie-counter-web/static/js/main.754382b5.chunk.js"
+  },
+  {
+    "revision": "ce95f5eb40b935bd10d3",
+    "url": "/calorie-counter-web/static/js/runtime-main.7eded731.js"
+  },
+  {
+    "revision": "e0257ee3678dae7bf6c5b206527ebadf",
+    "url": "/calorie-counter-web/static/media/mario.e0257ee3.ttf"
+  }
+]);
