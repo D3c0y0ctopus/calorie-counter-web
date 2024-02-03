7428737a51f9adfa91fab0f316f9def577d8cc2d
D3c0y0ctopus <chris.ramsden85@gmail.com>
2024-02-03 12:49:41 +0000

    commit

--- src/TotalCalories.js
+++ src/TotalCalories.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 function TotalCalories({ totalCalories }) {
-  // Determine the class based on totalCalories
+  // Determine the class based on totalCalories value
   const caloriesClass = totalCalories > 2400 ? 'calories-high' : 'calories-normal';
 
   return (
     <div className="total-calories-container">
-      <span className="total-calories-label">Total Calories:</span>
-      <span className={`total-calories-value ${caloriesClass}`}>{totalCalories}</span>
+      <div className="total-calories-label">Total Calories</div>
+      {/* Apply the dynamically determined class */}
+      <div className={`total-calories-value ${caloriesClass}`}>{totalCalories}</div>
     </div>
   );
 }
 
 export default TotalCalories;
+
+
